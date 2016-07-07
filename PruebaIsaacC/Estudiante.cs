a67394d5262c1276d71717cb007cce25d473de37
IsaacCR <isaaccivan@gmail.com>
2016-07-07 10:32:15 -0600

    Agregando Proyecto a repositorio

---
+++ PruebaIsaacC/Estudiante.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaIsaacC
+{
+    public partial class Estudiante : Form
+    {
+        public Estudiante()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Estudiante_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
